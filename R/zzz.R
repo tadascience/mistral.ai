d900e12271451bc22e9b808a92d6aab26ec709e3
Romain Francois <romain@purrple.cat>
2024-03-08 12:55:30 +0100

    + resp_chat()

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
-#' @import  rlang
-#' @import  cli
-#' @import  httr2
+#' @import rlang
+#' @import cli
+#' @import httr2
+#' @import tibble
+#' @importFrom purrr map_dfr
 NULL
 
 mistral_base_url <- "https://api.mistral.ai"
