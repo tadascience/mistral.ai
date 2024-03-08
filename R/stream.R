232766437ecee347461783624cf672886ca9b4e6
James Wade <github@jameshwade.com>
2024-03-08 07:47:51 -0500

    a start at streaming

--- R/stream.R
+++ R/stream.R
@@ -1,3 +1,22 @@
-stream <- function() {
+stream <- function(text, model = "mistral-tiny") {
+  if (!(model %in% models())) {
+    cli::cli_abort("The model ", model, " is not available.",
+      "i" = "Please use the {.code models()} function to see the available models."
+    )
+  }
 
+  req <- req_chat(text, model, stream = TRUE)
+  resp <- req_perform_stream(req,
+    callback = stream_callback,
+    round = "line",
+    buffer_kb = 0.01
+  )
+
+  invisible(resp)
+}
+
+
+stream_callback <- function(x) {
+  cat(rawToChar(x))
+  TRUE
 }
