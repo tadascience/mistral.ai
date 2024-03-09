52e909bce2f6616fa9e900705170512cf7424d08
Romain Francois <romain@purrple.cat>
2024-03-09 11:01:23 +0100

    using cli_abort(parent=)

---
+++ R/httr2.R
@@ -0,0 +1,26 @@
+req_mistral_perform <- function(req, error_call = caller_env()) {
+
+  url <- req$url
+  handle_req_perform_error <- function(err) {
+    bullets <- c(x = "with endpoint {.url {url}}")
+
+    if (!inherits(err, "error_mistral_req_perform")) {
+      bullets <- c(
+        bullets,
+        i = "Make sure your api key is valid {.url https://console.mistral.ai/api-keys/}",
+        i = "And set the {.envvar MISTRAL_API_KEY} environment variable",
+        i = "Perhaps using {.fn usethis::edit_r_environ}"
+      )
+    }
+
+    cli::cli_abort(
+      bullets, class = c("error_mistral_req_perform"),
+      call = error_call, parent = err
+    )
+  }
+
+  withCallingHandlers(
+    req_perform(req),
+    error = handle_req_perform_error
+  )
+}
