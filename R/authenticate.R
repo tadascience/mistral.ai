a5280d6ec4f7da16c26935ecdea51827feb18da7
Romain Francois <romain@purrple.cat>
2024-03-09 12:04:20 +0100

    simplify authenticate()

--- R/authenticate.R
+++ R/authenticate.R
@@ -1,11 +1,3 @@
-authenticate <- function(request, dry_run = FALSE, error_call = caller_env()){
-  key <- Sys.getenv("MISTRAL_API_KEY")
-  if (!is_true(dry_run) && identical(key, "")) {
-    cli_abort(call = error_call, c(
-      "Please set the {.code MISTRAL_API_KEY} environment variable",
-      i = "Get an API key from {.url https://console.mistral.ai/api-keys/}",
-      i = "Use {.code usethis::edit_r_environ()} to set the {.code MISTRAL_API_KEY} environment variable"
-    ))
-  }
-  req_auth_bearer_token(request, key)
+authenticate <- function(request){
+  req_auth_bearer_token(request, Sys.getenv("MISTRAL_API_KEY"))
 }
