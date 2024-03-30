1b10fc6e07a27e0cfe1f23a869ba29141a0f2ce1
Romain Francois <romain@tada.science>
2024-03-30 13:20:41 +0100

    Move handlers inside req_mistral_perform()

--- R/httr2.R
+++ R/httr2.R
@@ -1,38 +1,42 @@
 req_mistral_perform <- function(req, error_call = caller_env()) {
 
   url <- req$url
-  handle_req_perform_error <- function(err) {
 
-    resp <- err$resp
+  handle_invalid_model_error <- function(err, resp) {
     status <- resp_status(resp)
-    json <- resp_body_json(resp)
-    if (status == 400 && json$type == "invalid_model") {
+    if (status == 400 && resp_body_json(resp)$type == "invalid_model") {
       model <- req$body$data$model
       cli_abort(c(
         "Invalid mistrai.ai model {.emph {model}}.",
         i = "Use one of {.or {models()}}."
       ), call = error_call)
-    } else {
-      bullets <- c(x = "with endpoint {.url {url}}")
+    }
+  }
 
-      if (!inherits(err, "error_mistral_req_perform")) {
-        bullets <- c(
-          bullets,
-          i = "Make sure your api key is valid {.url https://console.mistral.ai/api-keys/}",
-          i = "And set the {.envvar MISTRAL_API_KEY} environment variable",
-          i = "Perhaps using {.fn usethis::edit_r_environ}"
-        )
-      }
+  handle_req_perform_error <- function(err, resp) {
+    bullets <- c(x = "with endpoint {.url {url}}")
 
-      cli_abort(
-        bullets, class = c("error_mistral_req_perform"),
-        call = error_call, parent = err
+    if (!inherits(err, "error_mistral_req_perform")) {
+      bullets <- c(
+        bullets,
+        i = "Make sure your api key is valid {.url https://console.mistral.ai/api-keys/}",
+        i = "And set the {.envvar MISTRAL_API_KEY} environment variable",
+        i = "Perhaps using {.fn usethis::edit_r_environ}"
       )
     }
+
+    cli_abort(
+      bullets, class = c("error_mistral_req_perform"),
+      call = error_call, parent = err
+    )
   }
 
   withCallingHandlers(
     req_perform(req),
-    error = handle_req_perform_error
+    error = function(err) {
+      resp <- err$resp
+      handle_invalid_model_error(err, resp)
+      handle_req_perform_error(err, resp)
+    }
   )
 }
