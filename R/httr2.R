7e167f899c9e10b743dbf981339c1c8cbbf5e0b4
Romain Francois <romain@tada.science>
2024-04-06 23:35:23 +0200

    simplify

--- R/httr2.R
+++ R/httr2.R
@@ -1,48 +1,29 @@
 req_mistral_perform <- function(req, error_call = caller_env()) {
-  req_perform(req) %!% \(err) handle_mistral_error(err, req, error_call)
-}
 
-handle_mistral_error <- function(err, req, error_call) {
-  resp <- err$resp
-  status <- resp_status(resp)
+  handle_mistral_error <- function(err) {
+    resp <- err$resp
+    url <- req$url
+    status <- resp_status(resp)
 
-  handler <- if (status == 401) {
-    handle_unauthorized
-  } else if (status == 400 && resp_body_json(resp)$type == "invalid_model") {
-    handle_invalid_model
-  } else {
-    handle_other
-  }
-  handler(err, req, resp, error_call = error_call)
-}
+    bullets <- if (status == 401) {
+      c(
+        "Unauthorized {.url {url}}.",
+        i = "Make sure your api key is valid {.url https://console.mistral.ai/api-keys/}",
+        i = "And set the {.envvar MISTRAL_API_KEY} environment variable",
+        i = "Perhaps using {.fn usethis::edit_r_environ}"
+      )
+    } else if (status == 400 && resp_body_json(resp)$type == "invalid_model") {
+      c(
+        "Invalid mistrai.ai model {.emph {model}}.",
+        i = "Use one of {.or {models()}}."
+      )
+    } else {
+      "Error with {.url {url}}."
+    }
 
-handle_invalid_model <- function(err, req, resp, error_call = caller_env()) {
-  status <- resp_status(resp)
-  if (status == 400 && resp_body_json(resp)$type == "invalid_model") {
-    model <- req$body$data$model
-    cli_abort(c(
-      "Invalid mistrai.ai model {.emph {model}}.",
-      i = "Use one of {.or {models()}}."
-    ), call = error_call)
-  }
-}
-
-handle_unauthorized <- function(err, req, resp, error_call = caller_env()) {
-  status <- resp_status(resp)
-  url <- req$url
-
-  if (status == 401) {
-    bullets <- c(
-      "Unauthorized {.url {url}}.",
-      i = "Make sure your api key is valid {.url https://console.mistral.ai/api-keys/}",
-      i = "And set the {.envvar MISTRAL_API_KEY} environment variable",
-      i = "Perhaps using {.fn usethis::edit_r_environ}"
-    )
     cli_abort(bullets, call = error_call)
   }
+
+  req_perform(req) %!% handle_mistral_error
 }
 
-handle_other <- function(err, req, resp, error_call = caller_env()) {
-  url <- req$url
-  cli_abort("Error with {.url {url}}.", call = error_call, parent = err)
-}
