d7d079e2b03367982c3bb50ddcdf2e648b1f45e2
Romain Francois <romain@tada.science>
2024-04-20 13:40:33 +0200

    stream() also returns chat_tibble

--- R/stream.R
+++ R/stream.R
@@ -5,25 +5,42 @@
 stream <- function(..., model = "mistral-tiny", error_call = current_env()) {
   messages <- as_messages(..., error_call = error_call)
   req <- req_chat(messages, model, stream = TRUE, error_call = error_call)
-  resp <- req_perform_stream(req, callback = stream_callback, round = "line", buffer_kb = 0.01)
 
-  invisible(resp)
+  streamer <- mistral_stream()
+  resp <- req_perform_stream(req, callback = streamer$callback, round = "line", buffer_kb = 0.01)
+
+  tbl_req  <- list_rbind(map(messages, as_tibble))
+  tbl_resp <- tibble(
+    role = "assistant",
+    content = paste0(streamer$tokens, collapse = "")
+  )
+  tbl      <- list_rbind(list(tbl_req, tbl_resp))
+
+  class(tbl) <- c("stream_tibble", "chat_tibble", class(tbl))
+  attr(tbl, "resp") <- resp
+  invisible(tbl)
 }
 
-stream_callback <- function(x) {
-  txt <- rawToChar(x)
+mistral_stream <- function() {
+  tokens <- list()
 
-  lines <- str_split(txt, "\n")[[1]]
-  lines <- lines[lines != ""]
-  lines <- str_replace_all(lines, "^data: ", "")
-  lines <- lines[lines != "[DONE]"]
+  callback <- function(x) {
+    txt <- rawToChar(x)
 
-  tokens <- map_chr(lines, \(line) {
-    chunk <- fromJSON(line)
-    chunk$choices$delta$content
-  })
+    lines <- str_split(txt, "\n")[[1]]
+    lines <- lines[lines != ""]
+    lines <- str_replace_all(lines, "^data: ", "")
+    lines <- lines[lines != "[DONE]"]
 
-  cat(tokens)
+    tok <- map_chr(lines, \(line) {
+      json <- fromJSON(line)
+      json$choices$delta$content
+    })
+    tokens <<- c(tokens, tok)
+    cat(tok)
 
-  TRUE
+    TRUE
+  }
+
+  environment()
 }
